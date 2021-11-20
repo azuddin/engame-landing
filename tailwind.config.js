bec2dee1ebce579c16946a5d4d8d88a4fbaf0724
Ahmad Azuddin <exectraquasar@gmail.com>
2021-11-21 01:07:02 +0800

    finish home page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: "Lato",
+        montserrat: "Montserrat",
+        roboto: "Roboto",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
