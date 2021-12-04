396f0cd013084639e36d7e94b04ee73269f1739a
Ahmad Azuddin <exectraquasar@gmail.com>
2021-12-05 00:07:10 +0800

    finish `faq` page

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -3,8 +3,41 @@ import Head from "next/head";
 import { Section } from "@engame/components";
 import { FiChevronDown } from "react-icons/fi";
 import Image from "next/image";
+import { useState } from "react";
+
+const Collapsible = (props: {
+  title: string;
+  content: JSX.Element;
+  isOpen?: boolean;
+}): JSX.Element => {
+  const { title, content, isOpen = false } = props;
+  return (
+    <div className="flex flex-col bg-white rounded-lg border-gray-50 border overflow-hidden shadow-lg">
+      <div
+        className={
+          "flex justify-between items-center px-6 p-4" +
+          (isOpen ? " bg-black text-white" : "")
+        }
+      >
+        <p className="font-montserrat font-bold">{title}</p>
+        <div className="w-8 flex justify-end">
+          <FiChevronDown className={isOpen ? "transform rotate-180" : ""} />
+        </div>
+      </div>
+      {isOpen && <div className="content p-4">{content}</div>}
+    </div>
+  );
+};
 
 const FAQ: NextPage = () => {
+  const [openedCard, setOpenedCard] = useState([""]);
+  const handleClickCard = (key: string) => {
+    if (openedCard.includes(key)) {
+      setOpenedCard([]);
+    } else {
+      setOpenedCard([key]);
+    }
+  };
   return (
     <>
       <Head>
@@ -34,112 +67,166 @@ const FAQ: NextPage = () => {
           </div>
         </div>
       </Section>
-      <Section bgColor="bg-gray-300">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <Section>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-6 py-10">
           <div className="flex flex-col items-center">
-            <p>I’m a Gamer</p>
+            <p className="font-montserrat font-bold text-2xl pb-3">
+              I’m a Gamer
+            </p>
             <div className="border border-b-2 w-40 border-black mb-4"></div>
             <div className="w-full">
               <ul className="space-y-2">
-                <li>
-                  <div className="flex flex-col bg-white rounded overflow-hidden">
-                    <div className="flex justify-between items-center p-4 bg-black text-white">
-                      <p>Is the game free to play?</p>
-                      <FiChevronDown />
-                    </div>
-                    <div className="content p-4">
+                {[
+                  {
+                    title: "Is the game free to play?",
+                    content: (
                       <p>
-                        Our fees vary depending on the industry and average
-                        monthly turnover. We charge a small percentage on each
-                        successful transaction that uses Riipay. Please get in
-                        touch for more details on our fee structure.
+                        Est ex dolore qui ullamco duis non consectetur elit ad
+                        ullamco sit. Nostrud aute eu eu ullamco proident esse
+                        labore deserunt esse aute. Lorem sint cillum non aliquip
+                        veniam sunt pariatur aute est occaecat. Ex amet esse
+                        elit quis consequat reprehenderit fugiat. Ea ullamco
+                        cupidatat sunt et. Nulla enim anim ipsum ullamco nostrud
+                        consectetur est Lorem eu. Deserunt exercitation amet ea
+                        laborum mollit laboris.
                       </p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex flex-col bg-white rounded overflow-hidden">
-                    <div className="flex justify-between items-center p-4 bg-white text-black">
-                      <p>Is the game free to play?</p>
-                      <FiChevronDown />
-                    </div>
-                    <div className="content p-4 hidden">
+                    ),
+                  },
+                  {
+                    title: "How can we redeem from your voucher vendors?",
+                    content: (
                       <p>
-                        Our fees vary depending on the industry and average
-                        monthly turnover. We charge a small percentage on each
-                        successful transaction that uses Riipay. Please get in
-                        touch for more details on our fee structure.
+                        Officia voluptate pariatur do tempor nisi excepteur aute
+                        proident in fugiat excepteur. Reprehenderit est proident
+                        eu ad occaecat in consequat occaecat culpa commodo
+                        veniam non. Ipsum eu do deserunt ea aliqua nulla. Ut
+                        nulla pariatur cillum et velit officia esse. Ipsum
+                        incididunt cillum tempor officia aliqua reprehenderit
+                        laborum aliqua. Irure do excepteur ex consectetur elit
+                        laboris amet eiusmod proident aliquip.
                       </p>
-                    </div>
-                  </div>
-                </li>
+                    ),
+                  },
+                  {
+                    title: "How can we redeem from your voucher vendors?",
+                    content: (
+                      <p>
+                        Incididunt occaecat aute esse est quis dolore irure
+                        eiusmod aliquip minim proident quis. Ullamco id laboris
+                        qui sunt dolor sit anim do nostrud minim nostrud
+                        proident aliqua consectetur. Magna ut minim sunt laborum
+                        eiusmod Lorem pariatur adipisicing minim ea.
+                      </p>
+                    ),
+                  },
+                ].map((i, k) => (
+                  <li
+                    key={`gamer-${k}`}
+                    onClick={() => handleClickCard(`gamer-${k}`)}
+                  >
+                    <Collapsible
+                      title={i.title}
+                      content={i.content}
+                      isOpen={openedCard.includes(`gamer-${k}`)}
+                    />
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
           <div className="flex flex-col items-center">
-            <p>I’m a Brand owner</p>
+            <p className="font-montserrat font-bold text-2xl pb-3">
+              I’m a Brand owner
+            </p>
             <div className="border border-b-2 w-40 border-black mb-4"></div>
             <div className="w-full">
               <ul className="space-y-2">
-                <li>
-                  <div className="flex flex-col bg-white rounded overflow-hidden">
-                    <div className="flex justify-between items-center p-4 bg-white text-black">
-                      <p>Is the game free to play?</p>
-                      <FiChevronDown />
-                    </div>
-                    <div className="content p-4 hidden">
+                {[
+                  {
+                    title:
+                      "Can we have our product featured in the game as part of the game?",
+                    content: (
+                      <p>
+                        Est ex dolore qui ullamco duis non consectetur elit ad
+                        ullamco sit. Nostrud aute eu eu ullamco proident esse
+                        labore deserunt esse aute. Lorem sint cillum non aliquip
+                        veniam sunt pariatur aute est occaecat. Ex amet esse
+                        elit quis consequat reprehenderit fugiat. Ea ullamco
+                        cupidatat sunt et. Nulla enim anim ipsum ullamco nostrud
+                        consectetur est Lorem eu. Deserunt exercitation amet ea
+                        laborum mollit laboris.
+                      </p>
+                    ),
+                  },
+                  {
+                    title:
+                      "Can we have a fully customised Tap Master deployment with no other vendors in it?",
+                    content: (
+                      <p>
+                        Officia voluptate pariatur do tempor nisi excepteur aute
+                        proident in fugiat excepteur. Reprehenderit est proident
+                        eu ad occaecat in consequat occaecat culpa commodo
+                        veniam non. Ipsum eu do deserunt ea aliqua nulla. Ut
+                        nulla pariatur cillum et velit officia esse. Ipsum
+                        incididunt cillum tempor officia aliqua reprehenderit
+                        laborum aliqua. Irure do excepteur ex consectetur elit
+                        laboris amet eiusmod proident aliquip.
+                      </p>
+                    ),
+                  },
+                  {
+                    title:
+                      "Can we have a fully customised Tap Master deployment with no other vendors in it?",
+                    content: (
+                      <p>
+                        Incididunt occaecat aute esse est quis dolore irure
+                        eiusmod aliquip minim proident quis. Ullamco id laboris
+                        qui sunt dolor sit anim do nostrud minim nostrud
+                        proident aliqua consectetur. Magna ut minim sunt laborum
+                        eiusmod Lorem pariatur adipisicing minim ea.
+                      </p>
+                    ),
+                  },
+                  {
+                    title:
+                      "Does Engame provide marketing services to enhance our exposure?",
+                    content: (
                       <p>
                         Our fees vary depending on the industry and average
                         monthly turnover. We charge a small percentage on each
                         successful transaction that uses Riipay. Please get in
                         touch for more details on our fee structure.
                       </p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex flex-col bg-white rounded overflow-hidden">
-                    <div className="flex justify-between items-center p-4 bg-white text-black">
-                      <p>Is the game free to play?</p>
-                      <FiChevronDown />
-                    </div>
-                    <div className="content p-4 hidden">
-                      <p>
-                        Our fees vary depending on the industry and average
-                        monthly turnover. We charge a small percentage on each
-                        successful transaction that uses Riipay. Please get in
-                        touch for more details on our fee structure.
-                      </p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex flex-col bg-white rounded overflow-hidden">
-                    <div className="flex justify-between items-center p-4 bg-black text-white">
-                      <p>Is the game free to play?</p>
-                      <FiChevronDown />
-                    </div>
-                    <div className="content p-4">
-                      <p>
-                        Our fees vary depending on the industry and average
-                        monthly turnover. We charge a small percentage on each
-                        successful transaction that uses Riipay. Please get in
-                        touch for more details on our fee structure.
-                      </p>
-                    </div>
-                  </div>
-                </li>
+                    ),
+                  },
+                ].map((i, k) => (
+                  <li
+                    key={`brand-${k}`}
+                    onClick={() => handleClickCard(`brand-${k}`)}
+                  >
+                    <Collapsible
+                      title={i.title}
+                      content={i.content}
+                      isOpen={openedCard.includes(`brand-${k}`)}
+                    />
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
       </Section>
-      <Section bgColor="bg-gray-200">
-        <div className="flex flex-col items-center">
-          <p>Still need help?</p>
-          <p>Our team will be happy to help!</p>
-          <button className="px-2 py-1 text-base bg-black text-white rounded">
-            Play game Now
+      <Section>
+        <div className="border border-b-2"></div>
+        <div className="flex flex-col items-center py-10 space-y-10">
+          <p className="text-center font-montserrat font-extrabold text-4xl">
+            Still need help?
+          </p>
+          <p className="text-center font-montserrat text-3xl">
+            Our team will be happy to help!
+          </p>
+          <button className="w-full md:w-52 px-5 py-2 border border-black bg-black text-white rounded font-montserrat font-bold">
+            Get in touch
           </button>
         </div>
       </Section>
