24d82ab27f0c02b11bf3272a3d6c312cd0798afc
Ahmad Azuddin <exectraquasar@gmail.com>
2021-12-04 22:37:07 +0800

    finish `pricing` page

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Section } from "@engame/components";
 import Image from "next/image";
+import { FiCheckCircle } from "react-icons/fi";
 
 const Pricing: NextPage = () => {
   return (
@@ -40,82 +41,100 @@ const Pricing: NextPage = () => {
           </div>
         </div>
       </Section>
-      <Section bgColor="bg-gray-300">
-        <div className="flex flex-col items-center">
-          <p>Subscribe your plan now!</p>
-          <p>
+      <Section>
+        <div className="flex flex-col items-center py-10">
+          <p className="text-center md:text-left font-montserrat font-extrabold text-4xl mb-4">
+            Subscribe your plan now!
+          </p>
+          <p className="text-center max-w-lg font-lato font-medium text-xl capitalize mb-10">
             Interested to find out what Games as a Service can do to your
             business? Subscribe to our basic starter package for FREE today!
           </p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-          <div className="bg-white rounded border shadow p-4 flex flex-col justify-between">
-            <div className="flex flex-col">
-              <p>Basic Starter</p>
-              <p>
-                <span>FREE</span>/annual
-              </p>
-              <p>
-                Empowering users to manage their electronic health record in a
-                secure & interoperable manner
-              </p>
-              <ol className="list-decimal pl-4">
-                <li>Access to GaaS player base</li>
-                <li>Voucher distribution</li>
-                <li>Fixed game challenges</li>
-                <li>Free business package trial</li>
-              </ol>
-            </div>
-            <button className="w-full px-2 py-1 text-base bg-black text-white rounded">
-              Choose plan
-            </button>
-          </div>
-          <div className="bg-white rounded border shadow p-4 flex flex-col justify-between">
-            <div className="flex flex-col">
-              <p>Business</p>
-              <p>
-                <span>RM299</span>/annual
-              </p>
-              <p>
-                Empowering users to manage their electronic health record in a
-                secure & interoperable manner
-              </p>
-              <ol className="list-decimal pl-4">
-                <li>Access to GaaS player base</li>
-                <li>Voucher distribution</li>
-                <li>Voucher optimization</li>
-                <li>Featuring your brand in game</li>
-                <li>Featuring your product in game</li>
-                <li>Fixed game challenges</li>
-              </ol>
-            </div>
-            <button className="w-full px-2 py-1 text-base bg-black text-white rounded">
-              Choose plan
-            </button>
-          </div>
-          <div className="bg-white rounded border shadow p-4 flex flex-col justify-between">
-            <div className="flex flex-col">
-              <p>Professional</p>
-              <p>
-                <span>RM599</span>/annual
-              </p>
-              <p>
-                Empowering users to manage their electronic health record in a
-                secure & interoperable manner
-              </p>
-              <ol className="list-decimal pl-4">
-                <li>Access to GaaS player base</li>
-                <li>Voucher distribution</li>
-                <li>Voucher optimization</li>
-                <li>Featuring your brand in game</li>
-                <li>Featuring your product in game</li>
-                <li>Featuring your competition in game</li>
-                <li>Custom game challenges</li>
-              </ol>
-            </div>
-            <button className="w-full px-2 py-1 text-base bg-black text-white rounded">
-              Choose plan
-            </button>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-6 xl:gap-20">
+            {[
+              {
+                title: "Basic Starter",
+                price: "FREE",
+                desc: "Empowering users to manage their electronic health record in a secure & interoperable manner",
+                descList: [
+                  "Access to GaaS player base",
+                  "Voucher distribution",
+                  "Fixed game challenges",
+                  "Free business package trial",
+                ],
+              },
+              {
+                title: "Business",
+                price: "RM299",
+                desc: "Empowering users to manage their electronic health record in a secure & interoperable manner",
+                descList: [
+                  "Access to GaaS player base",
+                  "Voucher distribution",
+                  "Voucher optimization",
+                  "Featuring your brand in game",
+                  "Featuring your product in game",
+                  "Fixed game challenges",
+                ],
+                isPopular: true,
+              },
+              {
+                title: "Professional",
+                price: "RM599",
+                desc: "Empowering users to manage their electronic health record in a secure & interoperable manner",
+                descList: [
+                  "Access to GaaS player base",
+                  "Voucher distribution",
+                  "Voucher optimization",
+                  "Featuring your brand in game",
+                  "Featuring your product in game",
+                  "Featuring your competition in game",
+                  "Custom game challenges",
+                ],
+              },
+            ].map((i, k) => (
+              <div
+                key={`pricing-${k}`}
+                className="bg-white rounded-2xl border shadow-lg p-4 flex flex-col justify-between relative"
+              >
+                {i?.isPopular && (
+                  <div className="w-20 absolute top-0 right-0 overflow-hidden inline-block rounded-tr-2xl">
+                    <div className="h-20 bg-black transform rotate-45 origin-bottom-right flex-1 flex items-end -ml-8 px-8">
+                      <p className="text-center text-white capitalize font-montserrat font-bold text-xs">
+                        popular choice
+                      </p>
+                    </div>
+                  </div>
+                )}
+                <div className="flex flex-col space-y-4">
+                  <p className="text-left font-montserrat font-bold text-xl capitalize">
+                    {i.title}
+                  </p>
+                  <p className="text-left font-lato font-thin text-xl">
+                    <span className="font-montserrat font-bold text-3xl">
+                      {i.price}
+                    </span>
+                    /annual
+                  </p>
+                  <p className="text-left font-lato">{i.desc}</p>
+                  <ol>
+                    {i.descList.map((li, key) => (
+                      <li
+                        key={`descList-${k}`}
+                        className="flex space-x-2 text-left font-lato font-bold"
+                      >
+                        <span className="text-green-500 text-lg pt-1">
+                          <FiCheckCircle />
+                        </span>
+                        <p>{li}</p>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+                <button className="w-full px-2 py-1 mt-6 text-base bg-black text-white rounded border border-black font-montserrat font-bold">
+                  Choose plan
+                </button>
+              </div>
+            ))}
           </div>
         </div>
       </Section>
