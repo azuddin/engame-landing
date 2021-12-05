7df0bdbf08c45f7c1e4ea35b4f49a08406d05b2f
Ahmad Azuddin <exectraquasar@gmail.com>
2021-12-05 09:18:19 +0800

    refactor

---
+++ src/pages/pricing/payment.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@engame/components";
+import { PageLayoutProps } from "src/types";
+
+const Payment: PageLayoutProps = () => {
+  return <>@TODO: Payment Page</>;
+};
+
+Payment.layout = Layout;
+
+export default Payment;
