40ef2778cc9b9b5510137417856ec71d78131cbf
Ahmad Azuddin <exectraquasar@gmail.com>
2022-01-13 18:25:23 +0800

    remove config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    loader: "imgix",
-    path: "http://noop/", // change to your assets path. ie: http://s3-url-containing-my-images.com
-  },
 };
