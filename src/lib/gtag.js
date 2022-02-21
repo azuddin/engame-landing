953dcc4cc17d35ca98f50869932e308d1e166189
Ahmad Azuddin <exectraquasar@gmail.com>
2022-02-21 21:46:52 +0800

    change ga id

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "G-NR8SJ5W4MP";
+export const GA_TRACKING_ID = "UA-208663907-1";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
